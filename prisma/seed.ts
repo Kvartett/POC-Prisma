be13c8fcbc75dd7689ec44104c27d531d4891594
Luiz Pedroso <felipedevluiz@gmail.com>
2023-01-30 17:10:25 -0300

    feat: relation n:n seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,19 +1,35 @@
 import prisma from "../src/database/database.js";
 
 async function main() {
-    await prisma.categories.createMany({
-        data: [
-            {
-                name: "Action"
-            },
-            {
-                name: "RTS"
-            },
-            {
-                name: "FPS"
-            },
-        ]
-    })
+    let category = await prisma.categories.findFirst();
+    if (!category) {
+        await prisma.categories.createMany({
+            data: [
+                {
+                    name: "Action"
+                },
+                {
+                    name: "RTS"
+                },
+                {
+                    name: "FPS"
+                },
+            ]
+        });
+    };
+
+    let games = await prisma.games.findFirst();
+    if (!games) {
+        await prisma.games.createMany({
+            skipDuplicates: true,
+            data: [
+                { name: "The last of us", categoryId: 1},
+                { name: "Age of Empires 2", categoryId: 2},
+                { name: "Apex", categoryId: 3}
+            ]
+        });
+    };
+       
 }
 
 main()
